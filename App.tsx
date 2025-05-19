0cd3e2cd66b3f86f968815187b818a224162d378
Maria Rita da Silva Gomes <rita.gomes@academico.ifrn.edu.br>
2025-05-19 16:06:43 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+          <Text styles={styles.titulo}>Divertidamente 2</Text>
+          <Text styles={styles.text}>Diretor: Kelsey Mann</Text>
+          <Text styles={styles.text}>Gênero: Animação </Text>
+          <Text styles={styles.text}>Ano:  2024</Text>
+          <Image style={styles.image} source={require('./assets/divertidamente.jpg')} ></Image>
+      </View>
+      <View style={styles.container}>
+          <Text styles={styles.titulo}>Wish: o Poder dos Desejos</Text>
+          <Text styles={styles.text}>Diretor: Chris Buck, Fawn Veerasunthorn </Text>
+          <Text styles={styles.text}>Gênero: Animação </Text>
+          <Text styles={styles.text}>Ano:  2023</Text>
+          <Image style={styles.image} source={require('./assets/wish.jpeg')} ></Image>
+      </View>
+      <View style={styles.container}>
+          <Text styles={styles.titulo}>Elementos</Text>
+          <Text styles={styles.text}>Diretor: Peter Sohn</Text>
+          <Text styles={styles.text}>Gênero: Animação </Text>
+          <Text styles={styles.text}>Ano:  2023</Text>
+          <Image style={styles.image} source={require('./assets/elementos.jpeg')} ></Image>
+      </View>
+      <View style={styles.container}>
+          <Text styles={styles.titulo}>A pequena sereia</Text>
+          <Text styles={styles.text}>Diretor: Rob Marshall</Text>
+          <Text styles={styles.text}>Gênero: Animação </Text>
+          <Text styles={styles.text}>Ano:  2023</Text>
+          <Image style={styles.image} source={require('./assets/pequena.jpeg')} ></Image>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f6f8fa',
+    width: 300,
+    height: 200,
+    borderRadius: 20,
+    marginBottom: 20
   },
+  image:{
+    width: 250,
+    height: 400,
+    borderRadius: 20
+  },
+  titulo:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  text:{
+    fontSize: 15,
+    fontWeight: 'normal',
+    fontStyle: 'italic',
+    marginBottom: 5
+  }
+
 });
